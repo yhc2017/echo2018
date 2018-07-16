a3ecc67826b924067e6d9066031ecdf3d6bd2880
JXChow <JXChow>
2018-07-16 20:27:51 +0800

    上传项目

---
+++ app/src/main/java/com/echo/quick/quickly/MainActivity.java
@@ -0,0 +1,21 @@
+package com.echo.quick.quickly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author zhou-jx
+ * @time 2018/7/16 20:22*
+ * @description 自动创建的MainActivity
+ *
+**/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
