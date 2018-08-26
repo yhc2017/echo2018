c239d6e9bb5f2c2ba095107151dd9582722734a1
R-Allison <r-allison@foxmail.com>
2018-08-26 16:20:21 +0800

    设置新密码界面

---
+++ app/src/main/java/com/echo/quick/activities/RetrievePasswordActivity.java
@@ -0,0 +1,15 @@
+package com.echo.quick.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RetrievePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieve_password);
+
+    }
+
+}
